8a60a0b286fb017c9808bc47aa7e0054a54d7b51
Alexander Komarov <komar81@gmail.com>
2020-06-13 21:59:38 +0300

    Добавил пакет propTypes в проект. Добавил валидацию св-ва erroCount в компонент WelcomeScreen

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import propTypes from 'prop-types';
 import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
 
 const App = (props) => {
-// eslint-disable-next-line react/prop-types
   const {errorCount} = props;
   return (<WelcomeScreen errorCount={errorCount} />);
+};
+
+App.propTypes = {
+  errorCount: propTypes.number.isRequired,
 };
 
 export default App;
