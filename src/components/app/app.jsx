6785279c0888261b1b6e4c5ef79412c666bcb434
Alexander Komarov <komar81@gmail.com>
2020-06-17 16:23:28 +0300

    Добавил react-router. Добавил компоненты в статике: ArtistQuestionScreen, GenreQuestionScreen

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
+import ArtistQuestionScreen from '../artist-question-screen/artist-question-screen.jsx';
+import GenreQuestionScreen from '../genre-question-screen/genre-question-screen.jsx';
 
 const App = (props) => {
   const {errorCount} = props;
@@ -8,7 +11,21 @@ const App = (props) => {
     // console.log(`Welcome Button Pressed`);
   };
 
-  return (<WelcomeScreen errorCount={errorCount} onWelcomeButtonPressed={onWelcomeButtonPressed} />);
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <WelcomeScreen errorCount={errorCount} onWelcomeButtonPressed={onWelcomeButtonPressed} />
+        </Route>
+        <Route exact path="/dev-artist">
+          <ArtistQuestionScreen />
+        </Route>
+        <Route exact path="/dev-genre">
+          <GenreQuestionScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 App.propTypes = {
